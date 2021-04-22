70d8aaf1d6d2cc59bba6d3d4ff364ba3d3269766
Michael Lekrans <mlekrans@hotmail.com>
2021-04-22 09:10:36 +0200

    More tests.. time handling

--- Sources/MLMathMasterEngine/MLMathMasterEngine.swift
+++ Sources/MLMathMasterEngine/MLMathMasterEngine.swift
@@ -1,3 +1,331 @@
-struct MLMathMasterEngine {
-    var text = "Hello, World!"
+import Foundation
+
+
+
+// MARK: - Game Category:
+/// Specifies what type of operator to test for like .`add`, .`mult`
+enum MLMathMasterGameCategory {
+    case add
+}
+
+
+
+
+
+// MARK: - GameType
+/// Game Type: What type of game(test) we are doing, like .sequence (like tables), .random
+enum MLMathMasterGameType {
+    case sequence
+}
+
+
+
+
+
+
+// MARK: - GameData
+/// Game Data: Holds information about one game, like `category`, `type`, `base` and so on
+struct MLMathMasterGameData {
+    var category: MLMathMasterGameCategory
+    var type: MLMathMasterGameType
+    var base: [Int] = []
+}
+
+
+
+
+
+
+// MARK: - GameStatus
+enum MLMathMasterGameStatus {
+    case none, started, stopped
+}
+
+
+
+
+
+
+// MARK: - GameSettings
+/// Game settings: settings for the MLMathMaster game
+struct MLMathMasterGameSettings {
+    var noOfQuestions: Int = 10
+}
+
+
+
+
+
+
+// MARK: - Question
+/// Question struct: Holds the information about one question
+class MLMathMasterQuestion: Identifiable {
+    var id = UUID()
+    var value1: Int
+    var value2: Int
+    var result: MLMathMasterQuestionResult?
+    var active: Bool = false {
+        willSet {
+            if newValue == true {
+                startTime = .now()
+                stopTime = nil
+            } else {
+                stopTime = .now()
+            }
+        }
+    }
+    var startTime: DispatchTime?
+    var stopTime: DispatchTime?
+    
+    
+    init(value1: Int, value2: Int) {
+        self.value1 = value1
+        self.value2 = value2
+    }
+}
+
+
+
+
+
+// MARK: - QuestionResult
+/// Result struct containing information about a question
+struct MLMathMasterQuestionResult {
+    var answer: Int
+    var expectedAnswer: Int
+    var success: Bool {
+        answer == expectedAnswer
+    }
+}
+
+
+
+
+
+
+
+
+
+// MARK: - Engine
+/// The MLMathMaster game engine.
+/// The controller of the game.
+/// You initialize a new game by calling `func newGame()` and specify what type and category and so on you want to test
+class MLMathMasterEngine {
+    
+    // MARK: - private properties
+    var noOfFetchedQuestions: Int = 0
+    var currentQuestion: MLMathMasterQuestion? {
+        willSet {
+            print("WILL SET")
+            if currentQuestion != nil {
+                currentQuestion!.active = false
+                
+            }
+            newValue?.active = false
+        }
+    }
+    // MARK: - public properties
+    var gameData: MLMathMasterGameData?
+    var settings: MLMathMasterGameSettings
+    var questions: [MLMathMasterQuestion] = []
+    var unansweredQuestions: [MLMathMasterQuestion] = []
+    var status: MLMathMasterGameStatus = .none
+
+    // MARK: - Computed properties
+    var answeredQuestions: Int {
+        questions.count - unansweredQuestions.count
+    }
+    var noOfRightAnswers: Int {
+        var count = 0
+        questions.forEach { (question) in
+            if let result = question.result {
+                if result.success == true {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+    
+    init(settings: MLMathMasterGameSettings? = nil) {
+        self.settings = settings ?? MLMathMasterGameSettings()
+    }
+    
+    
+    
+    
+        
+    // MARK: - Private methods
+    
+    
+    /// Reset and regenerates new questions based on the `count` value. These questions are stored in the `self.questions` array
+    /// - Parameter count: No of questions to produce
+    private func generateNewQuestions(count: Int) {
+        questions = []
+        for i in 1...count {
+            questions.append(MLMathMasterQuestion(value1: getValue1(), value2: getValue2(index: i)))
+        }
+        /// value semantic so this is a full copy
+        self.unansweredQuestions = questions
+    }
+    
+
+    /// Generate a value for `value1` based on the `base` value in `gameData`.
+    /// If the `base` value is a single value we just return that value.. otherwise we randomize between the values in the `base` array
+    /// - Returns: <#description#>
+    private func getValue1() -> Int {
+        if let gameData = gameData {
+            if gameData.base.count == 1 {
+                return gameData.base[0]
+            } else {
+                return gameData.base[Int.random(in: 0...gameData.base.count-1)]
+            }
+        } else {
+            return 0
+        }
+    }
+    
+    
+    /// Generate a value for `value2` based on the `type` value in `gameData`
+    /// If it is a `.sequence` we just return the `index` supplied to the method otherwise (not sequence) we randomize the number
+    /// - Parameter index: <#index description#>
+    /// - Returns: <#description#>
+    private func getValue2(index: Int) -> Int {
+        if let gameData = gameData {
+            let value = gameData.type == .sequence ? index - 1: Int.random(in: 0...9)
+            return value
+        } else {
+            return 0
+        }
+    }
+    
+    
+    /// Remove the evaluated question from the list of unanswered questions
+    /// - Parameter question: the question to be removed
+    private func removeQuestionFromRemaining(question: MLMathMasterQuestion) {
+        let index = self.unansweredQuestions.firstIndex { q -> Bool in
+            q.id == question.id
+        }
+        if let index = index {
+            self.unansweredQuestions.remove(at: index)
+            if self.unansweredQuestions.count == 0 {
+                self.status = .stopped
+            }
+        }
+    }
+    
+    
+    /// Adds the result to the question.. the reason for this is that its value typed and I could not get the value back in a nice way..
+    /// - Parameters:
+    ///   - question: the question that will get a result struct
+    ///   - result: the result struct
+    func addResultToQuestion(question: MLMathMasterQuestion, result: MLMathMasterQuestionResult) {
+        let index = self.questions.firstIndex { (q) -> Bool in
+            q.id == question.id
+        }
+        if let index = index {
+                self.questions[index].result = result
+        }
+    }
+    
+    /// Evaluates the value by combining the two values in the question with the right operator specified by the gameData.category type
+    /// - Parameter question: <#question description#>
+    /// - Returns: <#description#>
+    func getResultOf(question: MLMathMasterQuestion) -> Int {
+        switch gameData?.category {
+        case .add:
+            return question.value1 + question.value2
+        default:
+            return -1
+        }
+    }
+    
+    
+    
+
+    
+    // MARK: - Public methods
+
+    
+    /// Start new game.
+    /// Creates a new GameData (MLMathMasterGameData) struct based.
+    /// Generates new questions base on the `type`, `category` and `base` values
+    /// If `noOfQuestions` is nil then the value from `settings.noOfQuestions` will be used
+    /// - Parameters:
+    ///   - category: MLMathMasterGameCategory. (the type of operation that is tested.. like .add)
+    ///   - type: MLMathMasterGameType: Wheter it is .sequense (like tables) or .random (for challange)
+    ///   - base: [Int], array of what numbers are the base for testing.. like [2] .. testing variants based on to like (2+3 if .add) (2*6 if .mult) and so on
+    ///   - noOfQuestions: Optional number of questions.. overrides the `settings.noOfQuestions` to produce
+    func newGame(
+        category: MLMathMasterGameCategory,
+        type: MLMathMasterGameType,
+        base: [Int],
+        noOfQuestions: Int? = nil) {
+        self.gameData = MLMathMasterGameData(category: .add, type: type, base: base)
+        generateNewQuestions(count: noOfQuestions ?? self.settings.noOfQuestions)
+        self.noOfFetchedQuestions = 0
+        self.status = .started
+    }
+    
+    
+    /// Returns questions specified by `count`. If there is not enough questions for `count` it will return what is left. If there are no more questions to return it will return `nil`
+    /// The private property `noOfFetchedQuestions` keeps track of how many questions have been delivered
+    /// The default value for `count` is one.. meaning that if count is not specified it will return one question
+    /// - Parameter count: The maximum numbers of questions we want to get
+    /// - Returns: Optional array of questions. Max will be `count`. If there is not enough questions left to satisfy `count` the remaing questions are returned. If there are no more questions to return it will return nil
+    func getQuestions(by count: Int = 1) -> [MLMathMasterQuestion]? {
+        guard questions.count > self.noOfFetchedQuestions else {
+            return nil
+        }
+        let nextIndex = self.noOfFetchedQuestions
+        /// get count questions OR if there is not enough get the rest
+        let actualCount = min(questions.count - self.noOfFetchedQuestions, count)
+        self.noOfFetchedQuestions += count
+
+        return Array(questions[nextIndex..<nextIndex + actualCount])
+    }
+    
+    
+    /// Get a single question. self.noOfFetchedQuestions will be incremented. If there are no more questions this method returns nil
+    /// - Returns: One question if there are questions left.. otherwise nil
+    func getQuestion() -> MLMathMasterQuestion? {
+        guard questions.count > self.noOfFetchedQuestions else {
+            currentQuestion = nil
+            return nil
+        }
+        
+        
+        let index = self.noOfFetchedQuestions
+        self.noOfFetchedQuestions += 1
+        
+        
+        return questions[index]
+    }
+    
+    
+    /// Evaluates a MLMathMasterQuestion by calling `getResultOf` that returns the answer
+    /// It then creates a result-struct (MLMathMasterQuestionResult), attaches the result to the question,
+    /// removes the question from `unansweredQuestions` and returns the result
+    /// - Parameters:
+    ///   - question: <#question description#>
+    ///   - answer: <#answer description#>
+    /// - Returns: <#description#>
+    func evaluateQuestion(question: inout MLMathMasterQuestion, answer: Int) -> MLMathMasterQuestionResult {
+        let correctAnswer = getResultOf(question: question)
+        let result = MLMathMasterQuestionResult(
+            answer: answer,
+            expectedAnswer: correctAnswer)
+        addResultToQuestion(question: question, result: result)
+        removeQuestionFromRemaining(question: question)
+        question.active = false
+        return result
+    }
+    
+    func activate(question: MLMathMasterQuestion) {
+        currentQuestion = question
+        question.active = true
+    }
+    
+    
+    
 }
--- Tests/MLMathMasterEngineTests/MLMathMasterEngineTests.swift
+++ Tests/MLMathMasterEngineTests/MLMathMasterEngineTests.swift
@@ -2,14 +2,365 @@ import XCTest
 @testable import MLMathMasterEngine
 
 final class MLMathMasterEngineTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MLMathMasterEngine().text, "Hello, World!")
+    func testCreateEngine() {
+        let engine = MLMathMasterEngine()
     }
 
+    func testCreatedEngineShouldHaveStatusNone() {
+        let engine = MLMathMasterEngine()
+        XCTAssert(engine.status == .none)
+    }
+
+    func testStartNewGameOfTypeAdd() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1])
+        XCTAssertNotNil(engine.gameData)
+        XCTAssertTrue(engine.gameData!.category == .add)
+    }
+    
+    func testStartNewGameShouldHaveStatusStarted() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1])
+        XCTAssert(engine.status == .started)
+    }
+
+    func testStartNewGameOfTypeAddHasRightNumberOfQuestions() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1])
+        XCTAssertTrue(engine.settings.noOfQuestions == 10)
+        XCTAssertTrue(engine.questions.count == 10, "no of questions should be 10 was \(engine.questions.count)")
+    }
+    
+    func testStartNewGameOfTypeAddWithChangedNumberOfQuestionsProduceRightNumberOfQuestions() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1], noOfQuestions: 74)
+        XCTAssert(engine.questions.count == 74, "question count should be 74, was \(engine.questions.count)")
+    }
+    
+    func testQuestionsHaveTwoValues() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1])
+        XCTAssertNotNil(engine.questions[0].value1)
+        XCTAssertNotNil(engine.questions[0].value2)
+    }
+    
+    func testQuestionGeneratedFromSequenceWithOneAsBase() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1])
+        for i in 0...9 {
+            print("testing \(i)")
+            XCTAssertTrue(engine.questions[i].value1 == 1 && engine.questions[i].value2 == i, "value1 should be 2 (was \(String(describing: engine.questions[i].value1)), value2 should be \(i) (was \(String(describing: engine.questions[i].value2)))")
+        }
+    }
+
+    func testQuestionGeneratedFromSequenceWithTwoAsBase() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        
+        for i in 0...9 {
+            print("testing \(i)")
+            XCTAssertTrue(engine.questions[i].value1 == 2 && engine.questions[i].value2 == i, "value1 should be 2 (was \(String(describing: engine.questions[i].value1)), value2 should be \(i) (was \(String(describing: engine.questions[i].value2)))")
+        }
+    }
+
+    
+    func testQuestionGeneratedFromSequenceWithOneAndFourAndSixAsBase() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [1, 4, 6], noOfQuestions: 100)
+
+        var noOfOnes: Int = 0
+        var noOfFours: Int = 0
+        var noOfSixs: Int = 0
+        var otherNumberFound = false
+
+        for i in 0...99 {
+            print("testing \(i)")
+            let question = engine.questions[i]
+            let value1 = question.value1
+            let value2 = question.value2
+            
+            switch value1 {
+            case 1:
+                noOfOnes += 1
+            case 4:
+                noOfFours += 1
+            case 6:
+                noOfSixs += 1
+            default:
+                otherNumberFound = true
+            }
+
+            XCTAssertTrue(value1 == 1 || value1 == 4 || value1 == 6  && engine.questions[i].value2 == i, "value1 should be 1, 4 or 6 (was \(String(value1)), value2 should be \(i) (was \(String(describing: value2)))")
+        }
+        XCTAssertFalse(otherNumberFound, "otherNumbersFound should be false.. was true")
+        XCTAssert(noOfOnes > 0, "NoOfOnes should be greater than 0.. was \(noOfOnes)")
+        XCTAssert(noOfFours > 0, "NoOfFours should be greater than 0.. was \(noOfFours)")
+        XCTAssert(noOfSixs > 0, "NoOfSixs should be greater than 0.. was \(noOfSixs)")
+    }
+    
+    func testGetGroupOfQuestionsSpecifiedByCount5() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2], noOfQuestions: 12)
+        
+        var noOfQuestions = 0
+        var noOfIterations = 0
+        var noOfQuestionsLastTime = 0
+        while let questions = engine.getQuestions(by: 5) {
+            noOfQuestionsLastTime = questions.count
+            noOfIterations += 1
+            print("Batch-----------------")
+            questions.forEach { question in
+                noOfQuestions += 1
+                print("question \(question.value1) + \(question.value2)")
+            }
+        }
+        XCTAssert(noOfQuestions == 12, "NoOfQuestions should be 12 was \(noOfQuestions)")
+        XCTAssert(noOfIterations == 3, "NoOfIterations should be 3 was \(noOfIterations)")
+        XCTAssert(noOfQuestionsLastTime == 2, "NoOfQuestionsLastTime should be 2, was \(noOfQuestionsLastTime)")
+    }
+
+    func testGetGroupOfQuestionsSpecifiedByCountDefault() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2], noOfQuestions: 12)
+        
+        var noOfQuestions = 0
+        var noOfIterations = 0
+        var noOfQuestionsLastTime = 0
+        while let questions = engine.getQuestions() {
+            noOfQuestionsLastTime = questions.count
+            noOfIterations += 1
+            print("Batch-----------------")
+            questions.forEach { question in
+                noOfQuestions += 1
+                print("question \(question.value1) + \(question.value2)")
+            }
+        }
+        XCTAssert(noOfQuestions == 12, "NoOfQuestions should be 12 was \(noOfQuestions)")
+        XCTAssert(noOfIterations == 12, "NoOfIterations should be 12 was \(noOfIterations)")
+        XCTAssert(noOfQuestionsLastTime == 1, "NoOfQuestionsLastTime should be 1, was \(noOfQuestionsLastTime)")
+    }
+
+    
+    func testGetSingleQuestions() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        var noOfQuestions = 0
+        while let _ = engine.getQuestion() {
+            noOfQuestions += 1
+        }
+        
+        XCTAssert(noOfQuestions == 10, "NoOfQuestions should be 10, was \(noOfQuestions)")
+    }
+    
+    func testEvaluateFirstQuestion() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        if var question = engine.getQuestion() {
+            let result = engine.evaluateQuestion(question: &question, answer: 2)
+            XCTAssert(result.success == true , "result.success should be true, was \(result.success)")
+            XCTAssertEqual(result.answer, result.expectedAnswer)
+        }
+    }
+    
+    func testEvaluatedQuestionHaveAResultType() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        var question = engine.getQuestion()
+        engine.evaluateQuestion(question: &question!, answer: 2)
+
+        let index = engine.questions.firstIndex { (q) -> Bool in
+            q.id == question!.id
+        }
+        XCTAssertNotNil(index)
+        XCTAssertNotNil(engine.questions[index!].result)
+    }
+    
+    func testEvaluateAllQuestionsOnAddAndBase2() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        var iteration = 0
+        while var question = engine.getQuestion() {
+            let answer = iteration + 2
+            iteration += 1
+            let result = engine.evaluateQuestion(question: &question, answer: answer)
+            XCTAssert(result.success == true , "result.success should be true, was \(result.success)")
+            XCTAssertEqual(result.answer, result.expectedAnswer)
+        }
+    }
+    
+    func testAllQuestionsAnsweredShouldSetStateToStopped() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        var iteration = 0
+        let unansweredQuestionsAtBeginning = engine.unansweredQuestions.count
+        
+        while var question = engine.getQuestion() {
+            let answer = iteration + 2
+            iteration += 1
+            let _ = engine.evaluateQuestion(question: &question, answer: answer)
+        }
+        
+        XCTAssert(engine.status == .stopped, "status should be .stopped .. was \(engine.status)")
+        XCTAssert(unansweredQuestionsAtBeginning == 10, "unansweredQuestionsAtBeginning should be 10, was \(unansweredQuestionsAtBeginning)")
+        XCTAssert(engine.unansweredQuestions.count == 0, "unansweredQuestions should be 0, was \(engine.unansweredQuestions.count)")
+
+    }
+    
+    func testAllStateAndPropertiesOnEngineShouldBeResetedWithNewGame() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+
+        var iteration = 0
+        
+        while var question = engine.getQuestion() {
+            let answer = iteration + 2
+            iteration += 1
+            let _ = engine.evaluateQuestion(question: &question, answer: answer)
+        }
+        
+        engine.newGame(category: .add, type: .sequence, base: [3])
+        XCTAssert(engine.status == .started)
+        XCTAssert(engine.questions.count == 10)
+        XCTAssert(engine.unansweredQuestions.count == 10)
+        XCTAssert(engine.noOfFetchedQuestions == 0)
+        XCTAssert(engine.answeredQuestions == 0)
+        XCTAssert(engine.noOfRightAnswers == 0)
+    }
+    
+    func testGameResultShouldShowOneWronglyAnsweredQuestionOf10() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        if var question = engine.getQuestion() {
+            engine.evaluateQuestion(question: &question, answer: 3000)
+        }
+        
+        XCTAssert(engine.answeredQuestions == 1, "answeredQuestions should be 1 was \(engine.answeredQuestions)")
+        XCTAssert(engine.noOfRightAnswers == 0, "noOfRightAnswers should be 0, was \(engine.noOfRightAnswers)")
+        
+    }
+
+    
+    func testGameResultShouldShowOneWronglyandOneRightAnsweredQuestionOf10() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        if var question = engine.getQuestion() {
+            print("Question \(question)")
+            engine.evaluateQuestion(question: &question, answer: 3000)
+        }
+        if var question = engine.getQuestion() {
+            print("Question \(question)")
+            engine.evaluateQuestion(question: &question, answer: 3)
+            print("Question after \(question)")
+        }
+
+        XCTAssert(engine.answeredQuestions == 2, "answeredQuestions should be 2 was \(engine.answeredQuestions)")
+        XCTAssert(engine.noOfRightAnswers == 1, "noOfRightAnswers should be 1, was \(engine.noOfRightAnswers)")
+    }
+    
+    func testActiveQuestionStartTimeOnQuestion() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        let question = engine.getQuestion()! // remember that it is valueType
+        XCTAssert(question.startTime == nil, "StartTime should be nil")
+        engine.activate(question: question)
+        XCTAssert(question.startTime != nil, "StartTime should have a value, was nil")
+        XCTAssert(question.stopTime == nil, "stopTime should be nil")
+    }
+
+    func testEvaluateActiveQuestionDeactivatesQuestion() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        var question = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: question)
+        XCTAssert(question.startTime != nil, "StartTime should NOT be nil")
+        XCTAssert(question.stopTime == nil, "StartTime should be nil")
+        engine.evaluateQuestion(question: &question, answer: 23)
+        XCTAssert(question.startTime != nil, "StartTime should have a value, was nil")
+        XCTAssert(question.stopTime != nil, "stopTime should have a value, was nil")
+    }
+
+    
+    func testActivateAnotherQuestionChangeStateOnPreviousQuestion() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        let question = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: question)
+        let question2 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: question2)
+        XCTAssertNotNil(question.startTime, "StartTime Should have a value.. was nil")
+        XCTAssertNotNil(question2.startTime, "StartTime Should have a value.. was nil")
+        XCTAssertFalse(question.active, "Running on question should be false")
+        XCTAssertTrue(question2.active, "Running on question2 should be true")
+        XCTAssertNotNil(question.stopTime, "Question stoptime should have a value, was nil")
+        XCTAssertNil(question2.stopTime, "Question2 stoptime should be nil")
+    }
+
+    func testGetQuestionBeyondScopeShouldDeactivateLastQuestionAndSetStopTime() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        let q1 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q1)
+        let q2 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q2)
+        let q3 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q3)
+        let q4 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q4)
+        let q5 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q5)
+        let q6 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q6)
+        let q7 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q7)
+        let q8 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q8)
+        let q9 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: q9)
+        let question10 = engine.getQuestion()! // remember that it is valueType
+        engine.activate(question: question10)
+        let question11 = engine.getQuestion() // remember that it is valueType
+        if question11 != nil {
+            engine.activate(question: question11!)
+        }
+        XCTAssertNil(question11, "question11 should be nil")
+        XCTAssertNotNil(question10.startTime, "StartTime Should have a value.. was nil")
+        XCTAssertFalse(question10.active, "Running on question should be false")
+        XCTAssertNotNil(question10.stopTime, "Question2 stoptime should NOT be nil")
+    }
+    
+    func testQuestionTime() {
+        let engine = MLMathMasterEngine()
+        engine.newGame(category: .add, type: .sequence, base: [2])
+        var q1 = engine.getQuestion()!
+        engine.activate(question: q1)
+        
+        let expectation = self.expectation(description: "Answer")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.1) {
+            engine.evaluateQuestion(question: &q1, answer: 3)
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        let timeDifference = q1.stopTime!.asSeconds - q1.startTime!.asSeconds
+        print("timeDifference = \(timeDifference)")
+        XCTAssert( timeDifference >= 3, "Time should be 3 sek or more.. was \(timeDifference)")
+    }
+
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testCreateEngine),
     ]
+}
+
+
+extension DispatchTime {
+    var asSeconds: Double {
+        Double(self.rawValue) / Double(1000000000)
+    }
 }
